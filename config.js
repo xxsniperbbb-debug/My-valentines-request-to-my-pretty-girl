704262d20c9a3d10686c24a2a07b7265b1c274f2
Behrad <xxsniperbbb@gmail.com>
2026-02-12 16:39:34 +0300

    Update config.js

--- config.js
+++ config.js
@@ -22,18 +22,18 @@ const CONFIG = {
     // Customize each question and its possible responses
     questions: {
         first: {
-            text: "Do you like me?",                                    // First interaction
+            text: "Do you LIKE me?",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
+            secretAnswer: "I don't like you, I LOVE YOU! ❤️❤️❤️"           // Secret hover message
         },
         second: {
             text: "How much do you love me?",                          // For the love meter
-            startText: "This much!",                                   // Text before the percentage
+            startText: "THIS MUCH??",                                   // Text before the percentage
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2025? 🌹", // The big question!
+            text: "Will you please be my Valentine on February 14th, 2026 ? 🌹", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
@@ -42,15 +42,15 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "WOOOOW YOU LOVE THAT MUCH??? 🥰💝",  // Shows when they go past 5000%
+        high: "TO IFINITY AND BEYOND BABY MAMA! 💝",              // Shows when they go past 1000%
+        normal: "AND BEYOND EVERYTHING! 🥰"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        title: "IM THE LUCKIEST PERSON IN THE WORLD! 🎉💝💖💝💓",
+        message: "I LOVE YOU SO MUCH MAMA, THANK YOU FOR ACCEPTING!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
